dd4ed111a670aa9b33020bd08c92e171a0edc987
Marin327 <108364779+Marin327@users.noreply.github.com>
2022-08-31 10:03:29 +0300

    Add files via upload

---
+++ tennisRankling.js
@@ -0,0 +1,36 @@
+function tennisRanklist(input) {
+    let tournamentsNumber = Number(input.shift());
+    let allPoints = Number(input.shift());
+    let currentPoints = 0;
+    let points = 0;
+    let winnerCounter = 0;
+    let stageInTournament = input.shift();
+ 
+    for (let i = 1; i <= tournamentsNumber; i++) {  
+        if (stageInTournament == 'W') {
+            points = 2000;
+            winnerCounter++;
+        } else if (stageInTournament == 'F') {
+            points = 1200;
+        } else if (stageInTournament == 'SF') {
+            points = 720;
+        }
+        allPoints += points;
+        currentPoints += points;
+        stageInTournament = input.shift();
+    }
+    let averagePoints = currentPoints / tournamentsNumber;
+    let winnerPercent = winnerCounter / tournamentsNumber * 100;
+
+    console.log(`Final points: ${allPoints}`);
+    console.log(`Average points: ${Math.floor(averagePoints)}`);
+    console.log(`${winnerPercent.toFixed(2)}%`);
+}
+
+tennisRanklist(["5", 
+"1400", 
+"F",  
+"SF", 
+"W", 
+"W", 
+"SF"]);
