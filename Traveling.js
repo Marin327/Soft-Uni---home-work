c449522d52736a756a444267f73978509af964e5
Marin Marinov <108364779+Marin327@users.noreply.github.com>
2022-09-08 16:15:04 +0300

    Add files via upload

---
+++ Traveling.js
@@ -0,0 +1,32 @@
+function solve(input) {
+let index = 0;
+let command = input[index];
+index++;
+
+while(command !== "End") {
+    let destination = command;
+    let budget = Number(input[index]);
+    index++;
+    let sum = 0;
+    while(sum < budget) {
+        let money = Number(input[index]);
+        index++;
+        sum += money
+    }
+    console.log(`Going to ${destination}!`);
+    command = input[index];
+    index++;
+  }
+
+}
+solve([
+"Greece",
+"1000",
+"200",
+"300",
+"150",
+"Spain",
+"300",
+"193",
+"423",
+"End"])
---
+++ clock.js
@@ -0,0 +1,13 @@
+function solve() {
+    for (let hour = 0; hour <= 23; hour++) {
+          for(let minutes = 0; minutes <= 59; minutes++) {
+            let result = '';
+            for(let seconds = 0; seconds <= 59; seconds++) {
+                result += `${hour} : ${minutes} : ${seconds}/n`
+            }
+                  console.log(result.slice(0, -1));
+           }
+       }
+   }
+
+solve()
