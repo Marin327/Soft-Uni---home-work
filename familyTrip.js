794a5a5e94b465dc0021493647084111c0d59fd2
Marin Marinov <108364779+Marin327@users.noreply.github.com>
2022-10-23 06:11:08 +0300

    Add files via upload

--- familyTrip.js
+++ familyTrip.js
@@ -1,16 +1,17 @@
 function solve(input) {
-    let budget = Number(input[0]); //· Бюджетът, с който разполагат – реално число в интервала [1.00 … 10000.00]
-    let nightsCnt = Number(input[1]); //· Брой нощувки – цяло число в интервала [0 … 1000]
-    let nightsPrice = Number(input[2]); //· Цена за нощувка – реално число в интервала [1.00 … 500.00]
-    let percent = Number(input[3]); //· Процент за допълнителни разходи – цяло число в интервала [0 … 100
+    let budget = Number(input[0]);
+    let nightCnt = Number(input[1]);
+    let nightPrice = Number(input[2]);
+    let percent = Number(input[3]);
 
-    if (nightsCnt > 7) {
-        nightsPrice *= 0.95;
+    if (nightCnt > 7) {
+        nightPrice = nightPrice * 0.95;
     }
-    let totalNightPrice = nightsCnt * nightsPrice;
-    let addinationalCosts = budget * (percent / 100);
 
-    let totalPrice = totalNightPrice + addinationalCosts;
+    let totalNightPrice = nightCnt * nightPrice;
+    let additionalCosts = budget * (percent / 100.0);
+
+    let totalPrice = totalNightPrice + additionalCosts;
 
     if (totalPrice > budget) {
         let moneyNeed = totalPrice - budget;
@@ -20,10 +21,9 @@ function solve(input) {
         console.log(`Ivanovi will be left with ${moneyLeft.toFixed(2)} leva after vacation.`);
     }
 }
-
-solve([
-    "800.50",
-    "8",
-    "100",
-    "2"
-])
+        solve([
+            "800.50",
+            "8",
+            "100",
+            "2"
+        ])
