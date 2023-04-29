4327b83867a427a519fea359631b3f8ff547a657
Marin Marinov <108364779+Marin327@users.noreply.github.com>
2023-04-29 20:46:28 +0300

    Add files via upload

--- spiralMatrix.js
+++ spiralMatrix.js
@@ -1,3 +1,44 @@
+function solve(row, col) {
+    let num = 1;
+    let finalArray = [];
+    for (let i = 0; i < row; i++) {
+        let currArray = [];
+        currArray.length = col;
+        finalArray.push(currArray);
+    }
+    let startRowIndex = 0;
+    let startColIndex = 0;
+
+    while(startRowIndex < row && startColIndex < col) {
+        for(let i = startColIndex; i < col; ++i) {
+            finalArray[startRowIndex][i] = num++;
+        }
+        startRowIndex++;
+        for(let i = startRowIndex; i < row; i++) {
+            finalArray[i][col - 1] = num++;
+        }
+        col--;
+     if(startRowIndex < row) {
+        for(let i = col - 1; i >= startColIndex; --i) {
+            finalArray[row - 1][i] = num++; 
+        }
+        row--;
+     }
+     if(startColIndex < col) {
+        for( let i = row - 1; i >= startRowIndex; --i) {
+            finalArray[i][startColIndex] = num++;
+        }
+        startColIndex++;
+     }
+    }
+    finalArray.forEach((row) => console.log(row.join(" ")));
+}
+solve(3, 3);
+solve(5, 5);
+
+//
+console.log("--------");
+//
 function foo(w, h) {
     let matrix = new Array(h).fill(new Array(w).fill([]));
     matrix = matrix.map(x => x.map(y => ""));
@@ -24,4 +65,5 @@ function foo(w, h) {
 
     return matrix.map(x => x.join(" ")).join("\n");
 }
+console.log(foo(3, 3));
 console.log(foo(5, 5));
